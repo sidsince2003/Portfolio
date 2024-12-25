923db51acc02234bbab01dfa304c97823e8ded3a
Siddharth Sharma <siddharyj28setu@gmail.comm>
2024-12-25 08:43:08 +0530

    second  commit

--- sanity.config.js
+++ sanity.config.js
@@ -2,7 +2,7 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
+import {schemaTypes} from './sanity-blogs/schemaTypes'
 
 export default defineConfig({
   name: 'default',
